150de0f1f3f75d859aa8487fd2a53e2f3654e043
Lukas Nielsen <moin@lukasnielsen.de>
2023-06-05 21:59:58 +0200

    move to vite

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
+
+export default defineConfig({
+	plugins: [
+		react(),
+		checker({
+			typescript: true,
+			eslint: { lintCommand: 'eslint "./src/**/*.{ts,tsx}"' },
+		}),
+	],
+	server: { open: true, port: 3000 },
+	build: { emptyOutDir: true, outDir: "./build" },
+});
