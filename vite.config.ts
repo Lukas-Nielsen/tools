de5c95e31362f22f55f09c8d0d1e67736bf1b687
Lukas Nielsen <moin@lukasnielsen.de>
2023-09-23 13:19:21 +0200

    add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
 	plugins: [
@@ -8,6 +9,16 @@ export default defineConfig({
 		checker({
 			typescript: true,
 			eslint: { lintCommand: 'eslint "./src/**/*.{ts,tsx}"' },
+		}),
+		VitePWA({
+			registerType: "autoUpdate",
+			manifest: false,
+			workbox: {
+				globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+			},
+			devOptions: {
+				enabled: true,
+			},
 		}),
 	],
 	server: { open: true, port: 3000 },
