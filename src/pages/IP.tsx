cbf9f2589f5fcde910bedde02443997487bff87f
Lukas Nielsen <moin@lukasnielsen.de>
2023-09-23 13:22:10 +0200

    auto update ip

--- src/pages/IP.tsx
+++ src/pages/IP.tsx
@@ -2,20 +2,27 @@ import React, { useEffect, useState } from "react";
 import Copy from "../functions/copy";
 
 const IP = () => {
-	const [ipv4, setIpv4] = useState<string>("unbekannt");
-	const [ipv6] = useState<string>("unbekannt");
+	const [ipv4, setIpv4] = useState<string>();
+	const [ipv6] = useState<string>();
 
 	useEffect(() => {
-		fetch("https://ip4.lukasnielsen.de")
-			.then((res) => {
-				if (res.ok) {
-					return res.text();
-				}
-				return "unbekant";
-			})
-			.then((text) => {
-				setIpv4(text);
-			}).catch;
+		const get = () => {
+			fetch("https://ip4.lukasnielsen.de")
+				.then((res) => {
+					if (res.ok) {
+						return res.text();
+					}
+					return "unbekant";
+				})
+				.then((text) => {
+					setIpv4(text);
+				})
+				.catch(() => setIpv4(undefined));
+		};
+		const interval = setInterval(get, 60 * 1000);
+		return () => {
+			clearInterval(interval);
+		};
 	}, []);
 	return (
 		<>
@@ -24,19 +31,19 @@ const IP = () => {
 				<div
 					className="copy"
 					onClick={() => {
-						Copy(ipv4, "IP kopiert.");
+						if (ipv4) Copy(ipv4, "IP kopiert.");
 					}}
 				>
-					{ipv4}
+					{ipv4 || "unbekannt"}
 				</div>
 				<div>IPv6 Adresse</div>
 				<div
 					className="copy"
 					onClick={() => {
-						Copy(ipv6, "IP kopiert.");
+						if (ipv6) Copy(ipv6, "IP kopiert.");
 					}}
 				>
-					{ipv6}
+					{ipv6 || "unbekannt"}
 				</div>
 			</div>
 		</>
