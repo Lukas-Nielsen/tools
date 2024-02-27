12eac69f69de5ed19ee46645a90f8ad77ae938bc
Lukas Nielsen <moin@lukasnielsen.de>
2024-02-27 22:15:37 +0100

    make mac format dynamic

--- src/pages/MAC.tsx
+++ src/pages/MAC.tsx
@@ -1,67 +1,74 @@
-import React, { ChangeEvent, useState } from "react";
-import { Card, Code, CopyButton, Input, Tooltip } from "@mantine/core";
+import React, { useState } from "react";
+import {
+	Card,
+	Code,
+	CopyButton,
+	SegmentedControl,
+	Stack,
+	TextInput,
+	Tooltip,
+} from "@mantine/core";
 
 const Mac = () => {
-	const [mac, setMac] = useState({
-		lower: "",
-		upper: "",
-		dash: "",
-		colon: "",
-	});
+	const [mac, setMac] = useState<string>("");
+	const [macCase, setCase] = useState<string>("lower");
+	const [sep, setSep] = useState<string>("");
 
-	const newFormat = (e: ChangeEvent<HTMLInputElement>) => {
-		let text = e.target.value;
-		if (text.length === 12 || text.length === 17) {
-			text = text.replaceAll(":", "");
-			text = text.replaceAll("-", "");
-			setMac({
-				lower: text.toLowerCase(),
-				upper: text.toUpperCase(),
-				dash: text
-					.split(/(.{2})/)
-					.filter((O) => O)
-					.join("-"),
-				colon: text
-					.split(/(.{2})/)
-					.filter((O) => O)
-					.join(":"),
-			});
+	const format = () => {
+		let r = mac;
+		if (r.length === 12 || r.length === 17) {
+			r = r.replaceAll(":", "");
+			r = r.replaceAll("-", "");
+
+			if (macCase === "lower") {
+				r = r.toLowerCase();
+			} else {
+				r = r.toUpperCase();
+			}
+
+			return r
+				.split(/(.{2})/)
+				.filter((O) => O)
+				.join(sep);
 		}
+		return "";
 	};
 
 	return (
 		<Card mb="xs">
 			<h3>MAC-Adresse formatieren</h3>
-			<Input placeholder="MAC-Adresse" onChange={newFormat} />
-			<h4>kleinbuchstaben</h4>
-			<CopyButton value={mac.lower}>
+			<Stack>
+				<TextInput
+					label="MAC-Adresse"
+					placeholder="MAC-Adresse"
+					value={mac}
+					onChange={(e) => setMac(e.currentTarget.value)}
+				/>
+				<SegmentedControl
+					size="xs"
+					value={macCase}
+					onChange={setCase}
+					data={[
+						{ label: "klein", value: "lower" },
+						{ label: "GROSS", value: "upper" },
+					]}
+				/>
+				<SegmentedControl
+					size="xs"
+					value={sep}
+					onChange={setSep}
+					data={[
+						{ label: "ohne", value: "" },
+						{ label: ":", value: ":" },
+						{ label: "-", value: "-" },
+					]}
+				/>
+			</Stack>
+			<h4>Ergebis</h4>
+			<CopyButton value={format()}>
 				{({ copied, copy }) => (
 					<Tooltip label={copied ? "MAC kopiert" : "MAC kopieren"}>
-						<Code onClick={copy}>{mac.lower}&nbsp;</Code>
-					</Tooltip>
-				)}
-			</CopyButton>
-			<h4>GROSSBUCHSTABEN</h4>
-			<CopyButton value={mac.upper}>
-				{({ copied, copy }) => (
-					<Tooltip label={copied ? "MAC kopiert" : "MAC kopieren"}>
-						<Code onClick={copy}>{mac.upper}&nbsp;</Code>
-					</Tooltip>
-				)}
-			</CopyButton>
-			<h4>mit "-"</h4>
-			<CopyButton value={mac.dash}>
-				{({ copied, copy }) => (
-					<Tooltip label={copied ? "MAC kopiert" : "MAC kopieren"}>
-						<Code onClick={copy}>{mac.dash}&nbsp;</Code>
-					</Tooltip>
-				)}
-			</CopyButton>
-			<h4>mit ":"</h4>
-			<CopyButton value={mac.colon}>
-				{({ copied, copy }) => (
-					<Tooltip label={copied ? "MAC kopiert" : "MAC kopieren"}>
-						<Code onClick={copy}>{mac.colon}&nbsp;</Code>
+						<Code onClick={copy}>{format()}&nbsp;</Code>
 					</Tooltip>
 				)}
 			</CopyButton>
