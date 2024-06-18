359d2597f258139f8445ed385a6b2a2c668d2f94
Lukas Nielsen <moin@lukasnielsen.de>
2024-06-18 08:27:57 +0200

    fix insecure randomness for generating passwords

--- src/pages/Password.tsx
+++ src/pages/Password.tsx
@@ -29,6 +29,7 @@ const Password = () => {
 				<IconRefresh
 					size={30}
 					onClick={() => setPassword(generatePassword())}
+					style={{ cursor: "pointer" }}
 				/>
 			</Group>
 			<h4>8 Zeichen</h4>
@@ -102,38 +103,49 @@ const generatePassword = () => {
 	};
 };
 
-const getPassword = (length: number) => {
-	let password = "";
-	for (let i = 0; i < length; i++) {
-		const choice = random(0, 3);
-		if (choice === 0) {
-			password += randomLower();
-		} else if (choice === 1) {
-			password += randomUpper();
-		} else if (choice === 2) {
-			password += randomSymbol();
-		} else if (choice === 3) {
-			password += random(0, 9);
-		} else {
-			i--;
-		}
-	}
-	return password;
-};
+// const getPassword = (length: number) => {
+// 	let password = "";
+// 	// for (let i = 0; i < length; i++) {
+// 	// 	const choice = random(0, 3);
+// 	// 	if (choice === 0) {
+// 	// 		password += randomLower();
+// 	// 	} else if (choice === 1) {
+// 	// 		password += randomUpper();
+// 	// 	} else if (choice === 2) {
+// 	// 		password += randomSymbol();
+// 	// 	} else if (choice === 3) {
+// 	// 		password += random(0, 9);
+// 	// 	} else {
+// 	// 		i--;
+// 	// 	}
+// 	// }
+// 	return password;
+// };
 
-const random = (min = 0, max = 1) => {
-	return Math.floor(Math.random() * (max + 1 - min) + min);
-};
+// const random = (min = 0, max = 1) => {
+// 	return Math.floor(
+// 		window.crypto.getRandomValues(new Uint32Array(1))[0] * (max + 1 - min) +
+// 			min,
+// 	);
+// };
 
-const randomLower = () => {
-	return String.fromCharCode(random(97, 122));
-};
+// const randomLower = () => {
+// 	return String.fromCharCode(random(97, 122));
+// };
 
-const randomUpper = () => {
-	return String.fromCharCode(random(65, 90));
-};
+// const randomUpper = () => {
+// 	return String.fromCharCode(random(65, 90));
+// };
 
-const randomSymbol = () => {
-	const symbols = "$%@#&!?-.{}()[]<>";
-	return symbols[random(0, symbols.length - 1)];
-};
+// const randomSymbol = () => {
+// 	const symbols = "$%@#&!?-.{}()[]<>";
+// 	return symbols[random(0, symbols.length - 1)];
+// };
+
+const getPassword = (
+	length = 20,
+	characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@-#$+*",
+) =>
+	Array.from(crypto.getRandomValues(new Uint32Array(length)))
+		.map((x) => characters[x % characters.length])
+		.join("");
