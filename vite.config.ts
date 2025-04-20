5468bbe589b5351673883363f93f51774063acfb
Lukas Nielsen <moin@lukasnielsen.de>
2025-04-20 21:16:45 +0200

    remove eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import checker from "vite-plugin-checker";
 import { VitePWA } from "vite-plugin-pwa";
+import oxlintPlugin from "vite-plugin-oxlint";
 
 export default defineConfig({
 	plugins: [
 		react(),
-		checker({
-			typescript: true,
-			eslint: { lintCommand: 'eslint "./src/**/*.{ts,tsx}"' },
-		}),
+		oxlintPlugin(),
 		VitePWA({
 			registerType: "autoUpdate",
-			workbox: {
-				globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
-			},
+			manifest: false,
+			workbox: { globPatterns: ["**/*.{js,css,html,ico,png,svg}"] },
+			devOptions: { enabled: true },
 		}),
 	],
+	resolve: {
+		alias: {
+			"@tabler/icons-react":
+				"@tabler/icons-react/dist/esm/icons/index.mjs",
+		},
+	},
 	base: "/",
 	server: { open: true, port: 3000 },
 	build: { emptyOutDir: true, outDir: "./build" },
