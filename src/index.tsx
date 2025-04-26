6608d65ed3d246a15187a1027ef68dc353465696
Lukas Nielsen <moin@lukasnielsen.de>
2025-04-26 13:31:49 +0200

    improve datetime

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@ import { createRoot } from "react-dom/client";
 import React from "react";
 import Main from "./Main";
 import { MantineProvider } from "@mantine/core";
-import "@mantine/core/styles.css";
+import "@mantine/core/styles.layer.css";
+import "@mantine/dates/styles.layer.css";
 
 const element = document.querySelector("#root");
 if (element) {
--- src/pages/DateTime.tsx
+++ src/pages/DateTime.tsx
@@ -1,53 +1,48 @@
-import { Card, Input } from "@mantine/core";
-import React, { ChangeEvent, useState } from "react";
+import { Card, NumberInput, Stack, Title } from "@mantine/core";
+import { DateInput, DateValue } from "@mantine/dates";
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import React, { useState } from "react";
+
+dayjs.extend(customParseFormat);
 
 const DateTime = () => {
 	const [unix, setUnix] = useState<string>(
 		(new Date().getTime() / 1000).toFixed(0),
 	);
-	const [date, setDate] = useState<string>(toIsoString(new Date()));
+	const [date, setDate] = useState<DateValue>(new Date());
 
-	const handleUnix = (e: ChangeEvent<HTMLInputElement>) => {
-		setUnix(e.target.value);
-		setDate(toIsoString(new Date(parseInt(e.target.value) * 1000)));
+	const handleUnix = (e: string | number) => {
+		const temp = Number(e);
+		setUnix(temp.toFixed(0));
+		setDate(new Date(temp * 1000));
 	};
 
-	const handleDate = (e: ChangeEvent<HTMLInputElement>) => {
-		setDate(e.target.value);
-		setUnix((new Date(e.target.value).getTime() / 1000).toFixed(0));
+	const handleDate = (e: DateValue) => {
+		if (e) {
+			setDate(e);
+			setUnix((e.getTime() / 1000).toFixed(0));
+		}
 	};
 
 	return (
-		<Card mb="xs">
-			<h3>Zeitrechner</h3>
-			<h4>Unixtimestamp</h4>
-			<Input
-				placeholder="Unixtimestamp"
+		<Card mb="xs" component={Stack}>
+			<Title order={3}>Zeitrechner</Title>
+			<NumberInput
 				value={unix}
 				onChange={handleUnix}
+				placeholder="Unixtimestamp"
+				label="Unixtimestamp"
+				hideControls
 			/>
-			<h4>Datum & Uhrzeit</h4>
-			<Input type="datetime-local" value={date} onChange={handleDate} />
+			<DateInput
+				valueFormat="DD.MM.YYYY HH:mm:ss"
+				value={date}
+				onChange={handleDate}
+				label="Datum & Uhrzeit"
+			/>
 		</Card>
 	);
 };
 
 export default DateTime;
-
-const toIsoString = (date: Date) => {
-	const pad = (num: number) => (num < 10 ? "0" : "") + num;
-
-	return (
-		date.getFullYear() +
-		"-" +
-		pad(date.getMonth() + 1) +
-		"-" +
-		pad(date.getDate()) +
-		"T" +
-		pad(date.getHours()) +
-		":" +
-		pad(date.getMinutes()) +
-		":" +
-		pad(date.getSeconds())
-	);
-};
