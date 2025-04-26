f784a21e423b6d2469e85247964a59e318d93ae4
Lukas Nielsen <moin@lukasnielsen.de>
2025-04-26 13:52:27 +0200

    remove comment

--- src/pages/Password.tsx
+++ src/pages/Password.tsx
@@ -111,45 +111,6 @@ const generatePassword = () => {
 	};
 };
 
-// const getPassword = (length: number) => {
-// 	let password = "";
-// 	// for (let i = 0; i < length; i++) {
-// 	// 	const choice = random(0, 3);
-// 	// 	if (choice === 0) {
-// 	// 		password += randomLower();
-// 	// 	} else if (choice === 1) {
-// 	// 		password += randomUpper();
-// 	// 	} else if (choice === 2) {
-// 	// 		password += randomSymbol();
-// 	// 	} else if (choice === 3) {
-// 	// 		password += random(0, 9);
-// 	// 	} else {
-// 	// 		i--;
-// 	// 	}
-// 	// }
-// 	return password;
-// };
-
-// const random = (min = 0, max = 1) => {
-// 	return Math.floor(
-// 		window.crypto.getRandomValues(new Uint32Array(1))[0] * (max + 1 - min) +
-// 			min,
-// 	);
-// };
-
-// const randomLower = () => {
-// 	return String.fromCharCode(random(97, 122));
-// };
-
-// const randomUpper = () => {
-// 	return String.fromCharCode(random(65, 90));
-// };
-
-// const randomSymbol = () => {
-// 	const symbols = "$%@#&!?-.{}()[]<>";
-// 	return symbols[random(0, symbols.length - 1)];
-// };
-
 const getPassword = (
 	length = 20,
 	characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@-#$+*",
