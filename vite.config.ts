017222dd8e44fedac3d0765f56f76f42dd77e972
Lukas Nielsen <moin@lukasnielsen.de>
2026-04-11 16:36:23 +0200

    migrate to vp

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from "vite-plus";
 import oxlintPlugin from "vite-plugin-oxlint";
 
 export default defineConfig({
+	staged: {
+		"*": "vp check --fix",
+	},
+	lint: { options: { typeAware: true, typeCheck: true } },
+	fmt: {
+		proseWrap: "always",
+		singleQuote: false,
+		tabWidth: 4,
+		useTabs: true,
+		printWidth: 80,
+		sortPackageJson: false,
+		ignorePatterns: ["node_modules/**", "build/**"],
+	},
 	plugins: [react(), oxlintPlugin()],
 	resolve: {
 		alias: {
