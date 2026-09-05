17e1c9fd897ac6e9cf391c6061e131e41891a42c
Lukas Nielsen <moin@lukasnielsen.de>
2026-09-05 14:50:58 +0200

    🚀 Deploy commit

--- src/func/client.ts
+++
@@ -1,5 +0,0 @@
-import { Client } from "@hyper-fetch/core";
-
-export const clientV4 = new Client({ url: "https://ip4.lukasnielsen.de" });
-
-export const clientV6 = new Client({ url: "https://ip6.lukasnielsen.de" });
