95ec3a89727c42cf3df9b9e62762d980b4a36b74
Lukas Nielsen <moin@lukasnielsen.de>
2026-09-05 16:06:22 +0200

    chore: only show totp result if any

--- src/pages/TOTP.tsx
+++ src/pages/TOTP.tsx
@@ -43,32 +43,34 @@ const TOTP = () => {
 			<Fieldset m={0} p={0} component={Stack} bd={0} bg="inherit">
 				<Title order={3}>TOTP</Title>
 				<TextInput key={form.key} {...form.getInputProps()} placeholder="TOTP Secret" label="TOTP Secret" />
-				<Stack gap="xs">
-					<Group justify="space-evenly">
-						<Box>
-							<Title order={4} ta="center">
-								Timer
-							</Title>
-							<Text c={timer < 5 ? "red" : "green"} ta="center">
-								{timer} Sek.
-							</Text>
-						</Box>
-						<Box>
-							<Title order={4} ta="center">
-								Code
-							</Title>
-							<CopyButton value={result}>
-								{({ copied, copy }) => (
-									<Tooltip label={copied ? "Code kopiert" : "Code kopieren"}>
-										<Code onClick={copy} className={classes.copy} ta="center">
-											{result.substring(0, 3)}&nbsp;{result.substring(3, 6)}
-										</Code>
-									</Tooltip>
-								)}
-							</CopyButton>
-						</Box>
-					</Group>
-				</Stack>
+				{result.length !== 0 && form.getValue().length !== 0 && (
+					<Stack gap="xs">
+						<Group justify="space-evenly">
+							<Box>
+								<Title order={4} ta="center">
+									Timer
+								</Title>
+								<Text c={timer < 5 ? "red" : "green"} ta="center">
+									{timer} Sek.
+								</Text>
+							</Box>
+							<Box>
+								<Title order={4} ta="center">
+									Code
+								</Title>
+								<CopyButton value={result}>
+									{({ copied, copy }) => (
+										<Tooltip label={copied ? "Code kopiert" : "Code kopieren"}>
+											<Code onClick={copy} className={classes.copy} ta="center">
+												{result.substring(0, 3)}&nbsp;{result.substring(3, 6)}
+											</Code>
+										</Tooltip>
+									)}
+								</CopyButton>
+							</Box>
+						</Group>
+					</Stack>
+				)}
 			</Fieldset>
 		</Card>
 	);
